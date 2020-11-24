74512fd3a64fe64d68f092357769227c29826049
Jernej <j@eee.si>
2020-11-24 17:08:15 +0100

    force deploy maybe

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 /* eslint-env node */
 'use strict';
-var BasePlugin = require('ember-cli-deploy-plugin');
-var fbTools = require('firebase-tools');
+const BasePlugin = require('ember-cli-deploy-plugin');
+const fbTools = require('firebase-tools');
 
 module.exports = {
   name: 'ember-cli-deploy-firebase',
 
-  createDeployPlugin: function(options) {
-    var DeployPlugin = BasePlugin.extend({
+  createDeployPlugin: function (options) {
+    const DeployPlugin = BasePlugin.extend({
       name: options.name,
 
-      upload: function(context) {
-        var outer = this;
-        var project = context.config.firebase.appName || context.config.fireBaseAppName;
-        var options = {
+      upload: function (context) {
+        const _self = this;
+        const project = context.config.firebase.appName || context.config.fireBaseAppName;
+        const options = {
           project: project,
           public: context.config.build.outputPath,
           message: (context.revisionData || {}).revisionKey,
-          verbose: context.ui.verbose
+          verbose: context.ui.verbose,
+          force: true
         };
         if (context.config.firebase.deployToken || process.env.FIREBASE_TOKEN) {
           options.token = context.config.firebase.deployToken || process.env.FIREBASE_TOKEN;
         }
-        return fbTools.deploy(options).then(function() {
-          outer.log('successful deploy!', {verbose: true});
-        }).catch(function(err) {
+        return fbTools.deploy(options).then(function () {
+          _self.log('successful deploy!', { verbose: true });
+        }).catch(function (err) {
           // handle error
-          outer.log('something bad happened oh no', { color: 'red' });
-          outer.log(err, { color: 'red' });
-          outer.log(err.stack, { color: 'red' });
+          _self.log('something bad happened oh no', { color: 'red' });
+          _self.log(err, { color: 'red' });
+          _self.log(err.stack, { color: 'red' });
         });
       }
     });
